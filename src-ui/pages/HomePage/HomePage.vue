bb48ea0fe2ae09d749890a7d460660d44f9278a7
Pawel_St <pawelst380@gmail.com>
2023-09-20 20:48:25 +0200

    Initial commit

---
+++ src-ui/pages/HomePage/HomePage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="text-h4 text-center">Welcome</div>
+  <div class="text-h5 text-center mt-3">
+    Select desired module from the menu on the left
+  </div>
+</template>
+
+<script setup>
+import { onMounted } from "vue";
+import ExampleService from "@/services/ExampleService";
+
+onMounted(() => {
+  const exampleService = new ExampleService();
+  exampleService
+    .getData()
+    .then((data) => {
+      console.log(data);
+    })
+    .catch((error) => {
+      console.log(error);
+    });
+});
+</script>
